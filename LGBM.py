be5bd589d028832e8df3b39bd06537662d924e23
yifor01 <yifor01@gmail.com>
2019-04-03 14:07:11 +0800

    Update LGBM.py

--- LGBM.py
+++ LGBM.py
@@ -6,15 +6,6 @@ from sklearn.model_selection import GridSearchCV
 ldata = lgb.Dataset(data,label=y)
 
 def LGBM_param_selection(X,y, nfolds):
-#    param = {'n_estimators':[600,900,1200,2400,2800],
-#            'learning_rate':[0.005,0.00764107,0.010,0.020,0.030,0.040,0.050,0.060,0.070,
-#                             0.080,0.090,0.1,0.15,0.2],
-#            'max_depth':[-1,1,2,3,4,5,6,7,8,9,10,11],
-#            'min_child_weight':[0,0.001,1,2,3,4,5],
-#            'metric': 'mae' }
-#
-#            'colsample_bytree':[0.7,0.8,0.9,1],
-#            'subsample':[0.7,0.8,0.9,1] ,
     param = {'n_estimators':[2800],
             'learning_rate':[0.005,0.01,0.02],
             'max_depth':[7,8,9,10],
@@ -34,26 +25,10 @@ def LGBM_param_selection(X,y, nfolds):
 LGBM_param_selection(data,y, 5 )
 
 
-#{'colsample_bytree': 0.6,
-# 'learning_rate': 0.01,
-# 'max_depth': 6,
-# 'min_child_weight': 0,
-# 'n_estimators': 2800,
-# 'subsample': 0.6}
-
-
-
-
 model_LGB = lgb.LGBMRegressor(n_estimators=2800,learning_rate=0.01,max_depth=6,
                               min_child_weight=0,metric='mac',subsample=0.3,colsample_bytree=0.5 ) 
-
 model_LGB.fit(data,y)
-
 model_LGB.predict(data)
-
-
-
-winsound.Beep(2500, 500)
 
 
 
@@ -67,8 +42,6 @@ model_LGB = lgb.LGBMRegressor(n_estimators=2800,learning_rate=0.01,max_depth=6,
 model_LGB.fit(train_x,train_y)
 mean_absolute_error( model_LGB.predict(valid_x)   , valid_y  )
 mean_absolute_error( model_LGB.predict(train_x)   , train_y  )
-
-
 
 
 model_LGB.fit(data,y)
