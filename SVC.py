e1e1cdba831d85b891b0d121c2fa185266424aa5
yifor01 <yifor01@gmail.com>
2019-04-03 14:03:01 +0800

    Add files via upload

---
+++ SVC.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 21:23:29 2018
+
+@author: yifor
+"""
+
+import winsound
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+
+def svc_Class_param_selection(X, y, nfolds):
+    Cs = [0.001, 0.01, 0.1, 1, 10]
+    gammas = [0.001, 0.01, 0.1, 1]
+    param_grid = {'C': Cs, 'gamma' : gammas}
+    grid_search = GridSearchCV(svm.SVC(kernel='rbf'), param_grid, cv=nfolds, 
+                               scoring='accuracy', verbose = 2,n_jobs=2)
+    grid_search.fit(X, y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+
+svc_Class_param_selection(class_data, class_y, 5 )
+
+
+winsound.Beep(2500, 500)
+
+
+
+
+ import os
+ os.chdir('D:\google下載\libsvm-3.23\libsvm-3.23\python')
+ from svmutil import *
+ 
+ 
+ 
+ y,x = svm_read_problem('../heart_scale')
+ m = svm_train(y[:200],x[:200],'-c 4')
+ p_label,p_acc,p_val=svm_predict(y[200:],x[200:],m)
+
+
+
+[c,g]=[2^-10,2^10]*[2^-10,2^10]
+
+svm_train( class_y.values,class_data,'-c 2' )
+
+svm_predict(class_pred)
---
+++ XG.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  1 02:24:55 2018
+
+@author: yifor
+"""
+##########################################################################
+# 2.LightGBM (桌電)
+import winsound
+import lightgbm as lgb
+from sklearn.model_selection import GridSearchCV
+
+ldata = lgb.Dataset(data,label=y)
+
+def LGBM_param_selection(X,y, nfolds):
+#    param = {'n_estimators':[600,900,1200,2400,2800],
+#            'learning_rate':[0.005,0.00764107,0.010,0.020,0.030,0.040,0.050,0.060,0.070,
+#                             0.080,0.090,0.1,0.15,0.2],
+#            'max_depth':[-1,1,2,3,4,5,6,7,8,9,10,11],
+#            'min_child_weight':[0,0.001,1,2,3,4,5],
+#            'metric': 'mae' }
+#
+#            'colsample_bytree':[0.7,0.8,0.9,1],
+#            'subsample':[0.7,0.8,0.9,1] ,
+    param = {'n_estimators':[2800],
+            'learning_rate':[0.005,0.01,0.02],
+            'max_depth':[7,8,9,10],
+            'min_child_weight':[0],
+            'colsample_bytree':[0.4,0.6,0.8],
+            'subsample':[0.4,0.6] 
+            }
+    
+    grid_search = GridSearchCV(estimator =lgb.LGBMRegressor(), 
+                               param_grid = param, scoring='neg_mean_absolute_error',
+                               cv=nfolds, verbose = 2,n_jobs=3)
+    grid_search.fit(X,y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+
+LGBM_param_selection(data,y, 5 )
+
+
+#{'colsample_bytree': 0.6,
+# 'learning_rate': 0.01,
+# 'max_depth': 6,
+# 'min_child_weight': 0,
+# 'n_estimators': 2800,
+# 'subsample': 0.6}
+
+
+
+
+model_LGB = lgb.LGBMRegressor(n_estimators=2800,learning_rate=0.01,max_depth=6,
+                              min_child_weight=0,metric='mac',subsample=0.3,colsample_bytree=0.5 ) 
+
+model_LGB.fit(data,y)
+
+model_LGB.predict(data)
+
+
+
+winsound.Beep(2500, 500)
+
+
+
+######################################
+# 測試(非正式)
+#model_LGB = lgb.LGBMRegressor(n_estimators=2300,learning_rate=0.05,max_depth=10,min_child_weight=1,metric='mac' ) 
+model_LGB = lgb.LGBMRegressor(n_estimators=2800,learning_rate=0.01,max_depth=6,
+                              min_child_weight=0,metric='mac',subsample=0.3,colsample_bytree=0.5 ) 
+
+# 檢查 overfitting
+model_LGB.fit(train_x,train_y)
+mean_absolute_error( model_LGB.predict(valid_x)   , valid_y  )
+mean_absolute_error( model_LGB.predict(train_x)   , train_y  )
+
+
+
+
+model_LGB.fit(data,y)
+
+LGBM_train = model_LGB.predict(data)
+LGBM_train[LGBM_train<0] = 0
+mean_absolute_error(LGBM_train,y)
+
+
+LGBM_pred = model_LGB.predict(pred)
+LGBM_pred[LGBM_pred<0] = 0
+
+submit_LGBM = output(  LGBM_pred )
+submit_LGBM.to_csv("submit_LGBM_8_8.csv",index=False)
+
+
+
+
+
+
+
+
